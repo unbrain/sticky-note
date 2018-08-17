93c1c07d5624711315200330a7c539104f08cf32
unbrain <marsorsun@gmail.com>
2018-08-17 11:20:33 +0800

    notes destory

--- routes/api.js
+++ routes/api.js
@@ -3,37 +3,72 @@ var router = express.Router();
 var Note = require('../model/note').Note
 
 /* GET listing. */
-router.get('/notes', function(req, res, next) {
-  Note.findAll({raw: true}).then((note) => {
-    res.send({status: 0, data: note})
+router.get('/notes', function (req, res, next) {
+  Note.findAll({
+    raw: true
+  }).then((note) => {
+    res.send({
+      status: 0,
+      data: note
+    })
   })
 });
 
-/* POST listing. */ 
+/* POST listing. */
 router.post('/notes/add', (req, res, next) => {
   console.log(1);
-  
+
   let note = req.body.note
-  Note.create({text: note}).then(()=>{
-    res.send({status: 0})
-  }).catch(()=>{
-    res.send({status:1, errorMsg: '数据库出错'})
+  Note.create({
+    text: note
+  }).then(() => {
+    res.send({
+      status: 0
+    })
+  }).catch(() => {
+    res.send({
+      status: 1,
+      errorMsg: '数据库出错'
+    })
   })
   console.log('add...', note);
-  
+
 });
 
-router.post('/notes/delete', function(req, res, next) {
-  console.log('delete...');
+router.post('/notes/delete', function (req, res, next) {
+  Note.destroy({
+    where: {
+      id: req.body.id
+    }
+  }).then(() => {
+    res.send({
+      status: 0
+    })
+  }).catch(() => {
+    sres.send({
+      status: 1,
+      error: '数据库错误'
+    })
+  })
 });
 
-router.post('/notes/edit', function(req, res, next) {
+router.post('/notes/edit', function (req, res, next) {
   let body = req.body
-  Note.update({text: body.note}, {where: {id: body.id).then(()=>{
+  Note.update({
+    text: body.note
+  }, {
+    where: {
+      id: body.id
+    }
+  }).then(() => {
     console.log(arguments)
-    res.send({status:0})
-  }).catch(()=>{
-    res.send({errorMsg: '服务器错误'})
+    res.send({
+      status: 0
+    })
+  }).catch(() => {
+    res.send({
+      errorMsg: '服务器错误'
+    })
   })
 });
 
